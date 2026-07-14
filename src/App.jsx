f5c62e02503f67c9046a5e2a241d7c6b5eac9f14
Shanmugam <shanmugam8489424109@gmail.com>
2026-07-14 12:34:58 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
+import Header from './components/Header.jsx'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import CalendarPage from './pages/Calendar'
